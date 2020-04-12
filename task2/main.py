4ef19c47ec195653f4e46e29e3df5d65927982b5
Leiv Andresen <leivandresen@leivs-macbook-pro.home>
2020-04-12 15:01:55 +0200

    Made simple multioutput linear regression for subtask3.

--- task2/main.py
+++ task2/main.py
@@ -7,11 +7,14 @@ import pandas as pd
 import copy as cp
 import shelve
 import os
+import time
 
 import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_validate
 from sklearn.linear_model import Ridge, BayesianRidge
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
 import tools
 
@@ -24,21 +27,7 @@ import tools
 """
 
 
-def algorithm():
-    # Description
-    desc = 'test1'
-
-    # Read inputs
-    data = pd.read_csv("data/train_features.csv")
-    print(data.head())
-
-    # # Write result
-    # print(w.reshape(-1, 1))
-    # write_csv(('task1b/data/result_' + desc + '_' + mode +'.csv'), w.reshape(-1, 1))
-    # print('Algorithm succeeded.')
-
-
-def inspect_data(vital_signs, tests):
+def inspect_vital_signs(vital_signs):
     # Set output options
     pd.set_option('display.max_rows', 100)
     pd.set_option('display.max_columns', None)
@@ -60,19 +49,19 @@ def inspect_data(vital_signs, tests):
     # print('NaNs in tests [%] \n', tests.isnull().sum() / len(tests))
 
 
-def load_data(filename):
+def load_features(filename):
     # Read inputs
     data = pd.read_csv(filename)
 
     # Split data into vital signs and tests
     vital_signs_ = data[['pid', 'Time', 'Age', 'Heartrate', 'SpO2', 'ABPs', 'ABPm', 'ABPd', 'RRate', 'Temp']]
     tests_ = data[['pid', 'Time', 'Age', 'EtCO2', 'PTT', 'BUN', 'Lactate', 'Hgb', 'HCO3', 'BaseExcess', 'Fibrinogen',
-                  'Phosphate', 'WBC', 'Creatinine', 'PaCO2', 'AST', 'FiO2', 'Platelets', 'SaO2', 'Glucose',
-                  'Magnesium', 'Potassium', 'Calcium', 'Alkalinephos', 'Bilirubin_direct', 'Chloride', 'Hct',
-                  'Bilirubin_total', 'TroponinI', 'pH']]
+                   'Phosphate', 'WBC', 'Creatinine', 'PaCO2', 'AST', 'FiO2', 'Platelets', 'SaO2', 'Glucose',
+                   'Magnesium', 'Potassium', 'Calcium', 'Alkalinephos', 'Bilirubin_direct', 'Chloride', 'Hct',
+                   'Bilirubin_total', 'TroponinI', 'pH']]
 
     return vital_signs_, tests_
-# def preprocess_vital_signs(vital_signs_data):
+
 
 def load_labels(filename):
     # Read inputs
@@ -86,40 +75,87 @@ def load_labels(filename):
 
     return y_vital_signs, y_tests_, y_sepsis
 
+
+def preprocess_vital_signs(vital_signs):
+    # Filling missing data with patient median or median of all patients
+    # vital_signs = vital_signs.head(n=1200)  # Split into a smaller set
+
+    # Group by patient
+    patient_data = vital_signs.groupby(by="pid")
+    median_all_patients = patient_data.median().median()
+
+    # Fill nans with median of patient or all patients
+    print("First patient before impunation: \n", patient_data.get_group(1))
+    patient_data_impunated = patient_data.head(0)
+    patient_features_list = []
+
+    print("\nImpunating data...")
+    starting_time, cts = time.time(), 0
+    for _, patient in patient_data:
+        # Fill all NaNs with patient median of variable
+        patient = patient.fillna(patient.median(skipna=True))
+        # Fill with hospital median if a variable is all NaNs
+        if patient.isnull().sum().sum():
+            patient = patient.fillna(median_all_patients)
+        patient_data_impunated = patient_data_impunated.append(patient)
+
+        # Write all features into a 1d array
+        # TODO Now taking time as well
+        patient_feature = patient.loc[:,
+                          ['Age', 'Heartrate', 'SpO2', 'ABPs', 'ABPm', 'ABPd', 'RRate',
+                           'Temp']].to_numpy().ravel()
+        pid = patient['pid'].mean()
+        patient_feature = np.append(pid, patient_feature)
+        patient_features_list.append(patient_feature)
+
+        # print progress
+        tools.progress_bar(cts, len(patient_data), start_time=starting_time)
+        cts = cts + 1
+
+    print("First patient after impunation: \n", patient_data_impunated.head(12))
+
+    # Matrix with all features
+    patient_features = np.asarray(patient_features_list)
+    return patient_features
+
+
 if __name__ == '__main__':
     # Load data
-    vital_signs, tests = load_data("data/train_features.csv")
+    vital_signs_raw, tests = load_features("data/train_features.csv")
+    y_vital_pd, y_tests_pd, y_sepsis_pd = load_labels("data/train_labels.csv")
+    y_vital = y_vital_pd.to_numpy()
+
     # Inspect data
-    # inspect_data(vital_signs, tests)
+    # inspect_vital_signs(vital_signs)
 
-    # # Perform a simple regression on vital signs filling missing data with patient median or median of all patients
-    # # Split into a smaller set
-    # vital_signs = vital_signs.head(n=12000)
-    # # Group by patient
-    # patient_data = vital_signs.groupby(by="pid")
-    # median_all_patients = patient_data.median().median()
-    #
-    # # Fill nans with median of patient or all patients
-    # print("First patient before impunation: \n", patient_data.get_group(1))
-    # patient_data_impunated = patient_data.head(0)
-    # for _, patient in patient_data:
-    #     # Fill all NaNs with patient median of variable
-    #     patient = patient.fillna(patient.median(skipna=True))
-    #     # Fill with hospital median if a variable is all NaNs
-    #     if patient.isnull().sum().sum():
-    #         patient = patient.fillna(median_all_patients)
-    #     patient_data_impunated = patient_data_impunated.append(patient)
-    #
-    # print("First patient after impunation: \n", patient_data_impunated.head(12))
+    # Preprocess vital signs
+    print("\n------ PREPROCESSING ------")
+    patient_features_vital = preprocess_vital_signs(vital_signs_raw)
 
-    # Load labels
-    y_vital, y_tests, y_sepsis = load_labels("data/train_labels.csv")
+    # Sort X and y by pid
+    ind = np.argsort(patient_features_vital[:, 0])
+    patient_features_vital = patient_features_vital[ind]
+    ind2 = np.argsort(y_vital[:, 0])
+    y_vital = y_vital[ind2]
+    # Check pid's match in rows
+    for index in range(np.shape(patient_features_vital)[0]):
+        if not (y_vital[index, 0] == patient_features_vital[index, 0]):
+            print('[Error] pids arent the same!')
+            print(y_vital[index, 0], patient_features_vital[index, 0])
 
     # Split into test and training sets
+    X_train, X_test, y_train, y_test = train_test_split(
+        patient_features_vital, y_vital, test_size=0.33, random_state=42)
 
-    # Train linear regression model on all vital signs or one
+    # Train linear regression model on all vital signs
+    print("\n------ TRAINING ------")
+    reg = LinearRegression().fit(X_train[:, 1:], y_train[:, 1:])
+    y_hat = reg.predict(X_test[:, 1:])
 
     # validate performance
+    print("\n------ VALIDATION ------")
+    print("Overall R2 score on test set:", reg.score(X_test[:, 1:], y_test[:, 1:]))
+    for column in range(np.shape(y_vital)[1] - 1):
+        print('Individual score:', r2_score(y_hat[:, column], y_test[:, column + 1]))
 
-
-
+    # TODO Compare to individual regressions
--- task2/tools.py
+++ task2/tools.py
@@ -1,6 +1,9 @@
 import numpy as np
 import csv
 import pandas as pd
+import sys
+import time
+import matplotlib.pyplot as plt
 
 
 def read_csv(filename):
@@ -27,3 +30,57 @@ def panda_check(filename):
     data = pd.read_csv(filename)
     print(data.head())
     print("NaN before cleaning: ", data.isnull().sum().sum())
+
+# # Display an image
+# def disp_img(img):
+#     try:
+#         if (img.data):
+#             cv.namedWindow('image', cv.WINDOW_AUTOSIZE)
+#             cv.imshow('image', img)
+#             cv.waitKey(0)
+#             cv.destroyAllWindows()
+#             return
+#         else:
+#             print('DISP_IMG: Image file contains no data.')
+#             return
+#     except TypeError:
+#         print('DISP_IMG: Invalid image file provided.')
+#         return
+
+# Progress bar to visualize all sorts of processes
+def progress_bar(counter, total_number_of_counts, start_time=None):
+    # Calculates the progress in percent by dividing counter by total_number_of_counts
+    # Then it adds a graphical representation of the percentage
+    percent = (("\r%d" % ((counter + 1) * 100 / total_number_of_counts) + "% | ")
+               + (int((counter + 1) * 10 / total_number_of_counts) * '=')
+               + (int((10 - (counter + 1) * 10 / total_number_of_counts)) * ' ') + " | ")
+
+    # If a starting time is provided then the progress bar will also output the time elapsed
+    # and an extrapolation of the remaining time
+    time_display = ''
+    if start_time is not None:
+        elapsed_time = time.time() - start_time
+        remaining_time = elapsed_time * total_number_of_counts / (counter + 1) - elapsed_time
+        time_display = (("%d" % (np.floor(elapsed_time/60))).zfill(2) + ":" + ("%d" % (elapsed_time%60)).zfill(2)
+                 + " | -" + ("%d" % (np.floor(remaining_time / 60))).zfill(2)
+                 + ":" + ('%d' % (remaining_time % 60)).zfill(2) + "  ")
+    print((percent + time_display), end=" ", flush=True)
+
+
+def read_csv(filename):
+    with open(filename, 'r') as readFile:
+        reader = csv.reader(readFile)
+        data = list(reader)
+        readFile.close()
+    print('[read_csv] Reading ', filename, ' done.')
+    return data
+
+
+def write_csv(filename, data):
+    with open(filename, 'w') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(data)
+    writeFile.close()
+    print("[write_csv] Wrote data to: ", filename)
+    return
+
